53a002c7aa45fb3a10dfb9ca472fe7657067204e
David Thibaudeau <dethibaudeau@gmail.com>
2013-10-24 23:19:12 -0400

    change to only allow for one argument in the, minor updates to main.cc

--- main.cc
+++ main.cc
@@ -1,3 +1,12 @@
-int main() {
+//#include </home/david/nademoii/pointers.h>
+#include <iostream>
 
-}	
+using namespace std;
+
+int main( int argc, char* argv[] ) {
+
+	cout << "hello world!" << endl;
+	cout << 100 << endl;	
+	return 0;
+	
+}
