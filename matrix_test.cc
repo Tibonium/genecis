0ea8bb2d85c9343183d0af1434f3db9ed82cc7cf
David Thibaudeau <dthibaudeau@aegistg.com>
2014-02-26 18:41:21 -0500

    Update matrix_test.cc
    tested new functions that had been added to matrix header

--- matrix_test.cc
+++ matrix_test.cc
@@ -4,20 +4,24 @@
 using namespace std ;
 
 int main() {
-	
+
 	matrix<int> mymatx(5,5) ;
-	matrix<int> omatx(5,1) ;
 	for(unsigned i=0; i<mymatx.nrow(); ++i) {
 		for(unsigned j=0; j<mymatx.ncol(); ++j) {
 			mymatx(i,j) = 5*i+j ;
 		}
 	}
+	matrix<int> omatx(5,1) ;
 	for(unsigned i=0; i<omatx.nrow(); ++i) {
 		for(unsigned j=0; j<omatx.ncol(); ++j) {
 			omatx(i,j) = i+j ;
 		}
 	}
-	
+
+//	bool result = typeid(double) == typeid(int) ;
+//	cout << "double == int: " ;
+//	cout << ((result) ? "true" : "false") << endl ;
+
 	cout << "mymatx: " << mymatx << endl ;
 	mymatx.transpose() ;
 	cout << "transpose: " << mymatx << endl ;
@@ -26,21 +30,36 @@ int main() {
 //	cout << "transpose: " << omatx << endl ;
 //	omatx = mymatx ;
 //	cout << "reassign: " << omatx << endl ;
-	
-//	matrix<int>* mtx = mymatx * omatx ;
-//	mtx = &omatx ;
-//	omatx.transpose() ;
-//	mtx *= omatx.transpose() ;
+
+	matrix<int> mtx = mymatx * omatx ;
+	mtx.transpose() ;
+	cout << "mtx: " << mtx << endl ;
+	mtx *= omatx ;
+	cout << "after *= mtx: " << mtx << endl ;
 //	omatx *= mymatx ;
+//	cout << "new omatx: " << omatx << endl ;
 //	mtx = &omatx ;
-	matrix<int>* mtx = new matrix<int>(2,2) ;
-	for(unsigned i=0; i<mtx->nrow(); ++i) {
-		for(unsigned j=0; j<mtx->ncol(); ++j) {
-			(*mtx)(i,j) = i+j ;
+//	matrix<int>* mtx = new matrix<int>(2,2) ;
+//	for(unsigned i=0; i<mtx->nrow(); ++i) {
+//		for(unsigned j=0; j<mtx->ncol(); ++j) {
+//			(*mtx)(i,j) = i*900+j*100 ;
+//		}
+//	}
+//	cout << "mtx: " << *mtx << endl ;
+//	(*mtx)(0,0) = 2 ;
+//	(*mtx)^2 ;
+//	cout << "mtx^2: " << *mtx << endl ;
+	
+	matrix<double> dmtx(2,2) ;
+	for(unsigned i=0; i<dmtx.nrow(); ++i) {
+		for(unsigned j=0; j<dmtx.ncol(); ++j) {
+			dmtx(i,j) = i+j*0.0234+1 ;
 		}
 	}
-	cout << "mtx: " << *mtx << endl ;
-//	(*mtx)(0,0) = 2 ;
-	(*mtx)^2 ;
-	cout << "mtx^2: " << *mtx << endl ;
+	cout << "dmtx: " << dmtx << endl ;
+	
+	omatx = omatx + omatx ;
+	cout << "+omatx: " << omatx << endl ;
+	dmtx.inverse() ;
+
 }
