c4a822f939dd740986f2d5f65e79335329008d76
David Thibaudeau <dethibaudeau@gmail.com>
2014-06-23 16:37:21 -0400

    Added the starting of a mathematical graph and basis for matching algorithm.

---
+++ algorithm/match.h
@@ -0,0 +1,32 @@
+#ifndef ALGORITHM_MATCH_H
+#define ALGORITHM_MATCH_H
+
+#include <vector>
+
+template <typename _T>
+class match {
+
+	public:
+	
+		/**
+		 * Uses the 
+		 */
+		void optimize() {
+	
+		}
+		
+		// Constructor
+		match(_T* s1, _T* s2) {
+		
+		}
+		
+		// Desctrutor
+		~match() {}
+		
+	private:
+	
+		
+
+};
+
+#endif
---
+++ math/graph.h
@@ -0,0 +1,23 @@
+#ifndef MATH_GRAPH_H
+#define MATH_GRAPH_H
+
+template <class _V, class _E>
+class graph {
+
+	public:
+		
+		// Constructor
+		graph( _V* vertices, _E* edges ) {
+		
+		}
+		
+		// Destructor
+		~graph() {}
+		
+	private:
+	
+		_V* _vertices ;
+		_E* _edges ;
+};
+
+#endif
