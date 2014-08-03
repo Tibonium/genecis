a5c50f0e995560b8706516536874f2fdd5bd0668
David Thibaudeau <dethibaudeau@gmail.com>
2014-08-03 12:05:45 -0400

    Updated code with comments and small changes. Renamed to proper format.

--- math/graph_v2.h
+++ math/graph_v2.h
@@ -2,10 +2,9 @@
  *	@file graph.h
  */
 
-#ifndef GENECIS_MATH_GRAPH_H
-#define GENECIS_MATH_GRAPH_H
+#ifndef GENECIS_MATH_GRAPH_V2_H
+#define GENECIS_MATH_GRAPH_V2_H
 
-#include "graph_node.h"
 #include <algorithm>
 #include <iostream>
 #include <map>
@@ -13,14 +12,14 @@
 namespace genecis {
 namespace math {
 
-class graph {
+class graph_v2 {
 
 	public:
 
 		/**
 		 * Construct a graph from a map
 		 */
-		graph( map<int,map>int,int> > g ) : _g(g),
+		graph_v2( map<int,map>int,int> > g ) : _g(g),
 			_num_nodes(_g.size())
 		{
 			_steps = new int[_num_nodes] ;
@@ -30,7 +29,7 @@ class graph {
 		/**
 		 * Construct a graph from a file
 		 */
-		graph( const char* filename ) {
+		graph_v2( const char* filename ) {
 			load_graph( filename ) ;
 			_num_nodes = _g.size() ;
 			_steps = new int[_num_nodes] ;
@@ -38,7 +37,7 @@ class graph {
 		}
 		
 		// Destructor
-		virtual ~graph() {
+		virtual ~graph_v2() {
 			if( _steps ) {
 				delete[] _steps ;
 			}
@@ -50,11 +49,11 @@ class graph {
 		 */
 		void scan_graph( int home, bool print=true ) {
 			cout << "Home: " << home << endl ;
-			cout << "Computing shortest travel distance to each node..." << endl ;
+			cout << "Computing shortest travel distance to each vertex..." << endl ;
 			char buff[64] ;
-			sprintf(buff, "%4s%20s%8s", "Node", "Distance", "Path") ;
+			sprintf(buff, "%4s%20s%8s", "Vertex", "Distance", "Path") ;
 			cout << buff << endl ;
-			find_path(home) ;
+			find_path( home ) ;
 			for(int i=0; i<_num_nodes; ++i) {
 				if( i != home ) {
 					if( _g[i]._distance < INF ) {
@@ -88,17 +87,18 @@ class graph {
 		
 	private:
 	
-		// Vector of the vertices of the graph with edges
-//		vector<graph_node> _g ;
+		/**
+		 * Member variables
+		 */
 		map<int,map<int,int> > _g ;
 		int* _steps ;
 		int _num_nodes ;
 		
-//		graph_node operator[] (int n) {
-//			return _g[n] ;
-//		}
-		
-		inline int smallest(int* arg) {
+		/**
+		 * Find the shortest distance from this vertex to
+		 * all of its neighbors.
+		 */
+		inline int smallest( int* arg ) {
 			int m = INF ;
 			int v = 0 ;
 			for(int i=0; i<_num_nodes; ++i) {
@@ -110,7 +110,11 @@ class graph {
 			return v ;
 		}
 		
-		void find_dist(int& current) {
+		/**
+		 * Recurrsively searches and finds the shortest distance
+		 * to each neighbor vertex from this vertex.
+		 */
+		void find_dist( int& current ) {
 			if( !_g[current]._visited ) {
 				int temp ;
 				_g[current]._path.push_back( current ) ;
@@ -133,8 +137,10 @@ class graph {
 			}
 		}
 		
-		// loads the graph from a file
-		// see graph.txt for correct format of file
+		/**
+		 * loads the graph from a file
+		 * see graph.txt for correct format of file
+		 */
 		bool load_graph( const char* filename ) {
 			ifstream file( filename ) ;
 			int nodes, a, b, v, e ;
@@ -169,7 +175,9 @@ class graph {
 			}
 		}
 		
-		// find the next value on the line
+		/**
+		 * find the next value on the line
+		 */
 		void search_entry( fstream& f, int& entry ) {
 			char c ;
 			while( true ) {
