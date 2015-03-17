0f117eeaab72d90c1d3297bc7ba73aa96385fbf7
David Thibaudeau <dthibaudeau@aegistg.com>
2015-03-17 10:35:42 -0400

    Modified bind to use alias_base and use new implementation. Added type casting ability with base type_cast

--- base/bind.h
+++ base/bind.h
@@ -6,28 +6,122 @@
 namespace genecis {
 namespace base {
 
-template<class T> class bind_base{};
+template<class T> class alias_base{};
 
-template<class T1, class T2>
-struct bind2 : public bind_base<bind2<T1,T2> >
+template<class R, class F>
+class f_arg0 : public alias_base<f_arg0<R,F> >
 {
 public:
-    typedef T1  bound_obj ;
-    typedef T2  bind_to ;
-    typedef bound_obj::result_type  result_type ;
-    typedef bound_obj::value_type   value_type ;
+    typedef R  result_type ;
     // constructor
-    bind2( const bound_obj& t1, const bind_to& t2 )
-        : __t1(t1), __t2(t2)
-    {}
-    static result_type apply( value_type a ) {
-        return bound_obj::apply( a ) ;
+    f_arg0( F f ) : __f(f) {}
+    result_type operator() ()
+    {
+        return __f() ;
     }
-    static bind_to get() { return __t2 ; }
+    result_type operator() () const
+    {
+        return __f() ;
+    }
 private:
-    bound_obj __t1 ;
-    bind_to __t2 ;
+    F f ;
 };
+
+template<class R, class A1, class F>
+class f_arg1 : public alias_base<f_arg1<R,A1,F> >
+{
+public:
+    typedef R result_type ;
+    // constructor
+    f_arg1( F f ) : __f(f) {}
+    result_type operator() ( A1 a1 )
+    {
+        return __f(a1) ;
+    }
+    result_type operator() ( A1 a1 ) const
+    {
+        return __f(a1) ;
+    }
+private:
+    F f ;
+};
+
+template<class R, class A1, class A2, class F>
+class f_arg2 : public alias_base<f_arg2<R,A1,A2,F> >
+{
+public:
+    typedef R result_type ;
+    // constructor
+    f_arg2( F f ) : __f(f) {}
+    result_type operator() ( A1 a1, A2 a2 )
+    {
+        return __f(a1,a2) ;
+    }
+    result_type operator() ( A1 a1, A2 a2 ) const
+    {
+        return __f(a1,a2) ;
+    }
+private:
+    F f ;
+};
+
+template<class R, class A1, class A2, class A3, class F>
+class f_arg3 : public alias_base<f_arg3<R,A1,A2,A3,F> >
+{
+public:
+    typedef R result_type ;
+    // constructor
+    f_arg3( F f ) : __f(f) {}
+    result_type operator() ( A1 a1, A2 a2, A3 a3 )
+    {
+        return __f(a1,a2,a3) ;
+    }
+    result_type operator() ( A1 a1, A2 a2, A3 a3 ) const
+    {
+        return __f(a1,a2,a3) ;
+    }
+private:
+    F f ;
+};
+
+template<class R, class A1, class A2, class A3, class A4, class F>
+class f_arg4 : public alias_base<f_arg4<R,A1,A2,A3,A4,F> >
+{
+public:
+    typedef R result_type ;
+    // constructor
+    f_arg4( F f ) : __f(f) {}
+    result_type operator() ( A1 a1, A2 a2, A3 a3, A4 a4 )
+    {
+        return __f(a1,a2,a3,a4) ;
+    }
+    result_type operator() ( A1 a1, A2 a2, A3 a3, A4 a4 ) const
+    {
+        return __f(a1,a2,a3,a4) ;
+    }
+private:
+    F f ;
+};
+
+template<class R, class A1, class A2, class A3, class A4, class A5, class F>
+class f_arg5 : public alias_base<f_arg5<R,A1,A2,A3,A4,A5,F> >
+{
+public:
+    typedef R result_type ;
+    // constructor
+    f_arg5( F f ) : __f(f) {}
+    result_type operator() ( A1 a1, A2 a2, A3 a3, A4 a4, A5 a5 )
+    {
+        return __f(a1,a2,a3,a4,a5) ;
+    }
+    result_type operator() ( A1 a1, A2 a2, A3 a3, A4 a4, A5 a5 ) const
+    {
+        return __f(a1,a2,a3,a4,a5) ;
+    }
+private:
+    F f ;
+};
+
 
 }   // end of namespace base
 }   // end of namespace genecis
---
+++ base/type_cast.h
@@ -0,0 +1,92 @@
+/**
+ * @file casting.h
+ * A class that defines many useful ways of casting
+ * types and redefining type casts on per-use basis.
+ */
+
+/***** Remove type templates *****/
+template<class T>
+struct remove_const {
+    typedef T type;
+};
+
+template<class T>
+struct remove_const<const T> {
+    typedef T type;
+};
+
+// remove volatile Template
+template<class T>
+struct remove_volatile {
+    typedef T type;
+};
+
+// remove volatile
+template<class T>
+struct remove_volatile<volatile T> {
+    typedef T type;
+};
+
+// remove pointer Template
+template<class T>
+struct remove_pointer {
+    typedef T type;
+};
+
+// remove pointer
+template<class T>
+struct remove_pointer<T*> {
+    typedef T type;
+};
+
+// remove reference Template
+template<class T>
+struct remove_reference {
+    typedef T type;
+};
+
+// remove reference
+template<class T>
+struct remove_reference<T&> {
+    typedef T type;
+};
+
+// Remove rvalue reference
+template<class T>
+struct remove_reference<T&&> {
+    typedef T type;
+};
+
+
+template<class T>
+struct remove_cv {
+    typedef typename remove_const<
+            typename remove_volatile<T>::type
+            >::type type;
+};
+
+/***** Add type templates *****/
+template<class T>
+struct add_const {
+    typedef const T type;
+};
+
+template<class T>
+struct add_volatile {
+    typedef volatile T type;
+};
+
+template<class T>
+struct add_cv {
+    typedef const volatile T type;
+};
+
+template<class T>
+struct add_reference {
+    typedef T& type;
+};
+
+template<class T>
+struct add_pointer {
+    typedef typename remove_reference<T>::type *type;
+};
