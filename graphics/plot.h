5303fa6fd553575b17514e781df77c69649a8a7a
David Thibaudeau <dthibaudeau@aegistg.com>
2015-09-09 10:05:14 -0400

    Added start of graphics package.

---
+++ graphics/plot.h
@@ -0,0 +1,25 @@
+/**
+ * @file plot.h
+ * Generic 2/3D plotting class
+ */
+ 
+#include <X11/Xlib.h>
+#include <X11/XKBlib.h>
+#include <GL/glx.h>
+#include <GL/glext.h>
+#include <GL/glu.h>
+
+/**
+ * 3D Plot Class
+ */
+class plot_2d {
+
+};  // end of plot_2d class
+
+
+/**
+ * 3D Plot Class
+ */
+class plot_3d {
+
+};  // end of plot_3d class
