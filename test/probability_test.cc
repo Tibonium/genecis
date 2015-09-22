0b1ed4c001d12a3c70589adba6d779749eb3ca7d
David Thibaudeau <dthibaudeau@aegistg.com>
2015-09-22 15:45:56 -0400

    Updated test for fun.

--- test/probability_test.cc
+++ test/probability_test.cc
@@ -7,7 +7,15 @@ USING_NAMESPACE(genecis::math)
 
 int main(int argv, char* argc[])
 {
-	cout << "11 choose 3: " << choose(11,3) << endl ; 
-	cout << "11 choose 3, with probability of success of 10%: " << binomial::pmf(11, 3, 0.1)*100.0 << "%" << endl ;
+	cout << "11 choose 3: " << choose(11, 3) << endl ;
+	cout << "11 choose 3, with probability of success at 10%: " << binomial::pmf(11, 3, 0.1)*100.0 << "%" << endl ;
+	cout << "110 choose 50, with probability of success at 10%: " << binomial::pmf(110, 50, 0.1)*100.0 << "%" << endl ;
+		
+	cout << "Chance for 1 5* in an 11-draw: " << binomial::pmf(11, 1, 0.1)*100.0 << "%" << endl ;
+	cout << "Chance for 2 5*s in an 11-draw: " << binomial::pmf(11, 2, 0.1)*100.0 << "%" << endl ;
+	
+	cout << "Chance for 1 5* in a 3-draw: " << binomial::pmf(3, 1, 0.1)*100.0 << "%" << endl ;
+	cout << "Chance for 2 5*s in a 3-draw: " << binomial::pmf(3, 2, 0.1)*100.0 << "%" << endl ;
+	cout << "Chance for 2 5*s in two 3-draws: " << binomial::pmf(6, 2, 0.1)*100.0 << "%" << endl ;
 
 }
